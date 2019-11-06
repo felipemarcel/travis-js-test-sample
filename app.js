8533919d4b29b1381cc465d079e83e0d51c97316
Felipe Marcel <felipemarcelqs@gmail.com>
2019-11-06 17:46:46 -0300

    Adding mongo model

--- app.js
+++ app.js
@@ -1,8 +1,33 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://felipe:felipe123@ds117868.mlab.com:17868/larg', {useNewUrlParser: true});
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+});
+
+var postSchema = new mongoose.Schema({
+    title: String 
+});
+
+var Post = new mongoose.model('Post', postSchema, 'posts');
+
+app.get('/posts/:id', function(req, res){
+
+});
+
+app.post('/posts', function(req, res){
+
+});
+
+app.get('/posts', function(req, res){
+    Post.find({}).lean().exec((err, posts) => {
+        res.json(posts.map(p => p.title));
+    });
 });
 
 app.listen(3000, function () {
